e13fb00f339197974bfaa4b6c6eacc76dac6f658
Charlie <charliekelly31415@gmail.com>
2019-04-15 16:19:43 -0400

    Add maps messages

---
+++ src/geo/messages.js
@@ -0,0 +1,6 @@
+export default {
+  locationSuccess: 'User location found',
+  locationFailure: 'Unable to retrieve location',
+  gmapApiSuccess: 'Google Maps API loaded successfully',
+  gmapApiFailure: 'Unable to load Google Maps API'
+}
