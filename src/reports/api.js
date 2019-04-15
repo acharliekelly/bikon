dbb4931cb912ccdf18fe25e4d1cedcbaa27e412d
Charlie <charliekelly31415@gmail.com>
2019-04-15 17:59:34 -0400

    Fill out API calls

--- src/reports/api.js
+++ src/reports/api.js
@@ -8,29 +8,66 @@ export const allReports = () => {
   })
 }
 
-export const filterReports = (params) => {
+export const myReports = (user) => {
   return axios({
     method: 'GET',
-    url: apiUrl + '/condreps?' + params
+    url: apiUrl + '/my-condreps',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
   })
 }
 
-export const myReports = (user) => {
-
-}
-
-export const openReport = (user, reportId) => {
-
+export const getReport = (user, reportId) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/condreps/' + reportId,
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
 }
 
 export const createReport = (user, params) => {
-
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/condreps',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: {
+      condition: params.condition,
+      geolat: params.latitude,
+      geolong: params.longitude,
+      reported_at: params.timestamp,
+      notes: params.notes
+    }
+  })
 }
 
 export const updateReport = (user, params) => {
-
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/condreps/' + params.id,
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: {
+      condition: params.condition,
+      geolat: params.latitude,
+      geolong: params.longitude,
+      reported_at: params.timestamp,
+      notes: params.notes
+    }
+  })
 }
 
 export const deleteReport = (user, reportId) => {
-
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/condreps/' + reportId,
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
 }
