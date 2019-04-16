086aec43473a70ea584f95a9349c8ec39393651e
Charlie <charliekelly31415@gmail.com>
2019-04-15 23:15:39 -0400

    Add Home component

---
+++ src/layout/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+const Home = () => (
+  <Fragment>
+    <h3>Home</h3>
+    <span className="todo">Put stuff here</span>
+  </Fragment>
+)
+
+export default Home
