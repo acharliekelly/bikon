ede943dd4e52109853d74dd9effc2e0aa332430c
Charlie <charliekelly31415@gmail.com>
2019-04-17 23:25:30 -0400

    experiments with maps

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react'
 
-// import BaseMap from '../geo/BaseMap'
+// import ReportMap from '../geo/ReportMap'
 
 const Home = () => (
   <Fragment>
     <h4>Home</h4>
-    { /* <BaseMap isMarkerShown /> */ }
   </Fragment>
 )
 
