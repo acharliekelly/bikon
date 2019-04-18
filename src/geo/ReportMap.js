40a1caecc1b9b5317125507e5d826ea8d43c8776
Charlie <charliekelly31415@gmail.com>
2019-04-17 23:24:40 -0400

    experiments with Maps

--- src/geo/ReportMap.js
+++ src/geo/ReportMap.js
@@ -1,41 +1,28 @@
 import React, { Component } from 'react'
 // import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
-import { GoogleMap, Marker } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
-import { getReport } from '../reports/api'
 // import messages from './messages'
 
+// Shows where a report occurred
 class ReportMap extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      report: null
-    }
-  }
-
-  componentDidMount () {
-    const { user } = this.props
-    const reportId = this.props.match.params.id
-    getReport(user, reportId)
-      .then(response => this.setState({ report: response.data.condrep }))
-      .catch(error => { console.log(error) })
-  }
-
   render () {
-    const { report } = this.state
-    if (!report) {
-      return 'Loading...'
-    } else {
-      return (
-        <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{ lat: 42.3542762, lng: -71.0538729 }}
-        >
-          <Marker position={{ lat: 42.3542762, lng: -71.0538729 }} />
-        </GoogleMap>
-      )
-    }
+    const gMap = withGoogleMap(props => (
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 42.3542762, lng: -71.0538729 }}
+      >
+        <Marker position={{ lat: 42.3542762, lng: -71.0538729 }} />
+      </GoogleMap>
+    ))
+    return (
+      <div>
+        <gMap
+          containerElement={ <div style={{ height: '500px', width: '500px' }} /> }
+          mapElement={ <div style={{ height: '100%' }} /> }
+        />
+      </div>
+    )
   }
 }
 
